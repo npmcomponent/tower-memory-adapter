213fcbee9a77dc7179d8e0f161bc913812d26e5d
Lance Pollard <lancejpollard@gmail.com>
2013-03-05 21:08:40 -0800

    need to come up with a standard set of operations to implement

--- index.js
+++ index.js
@@ -5,6 +5,11 @@
 
 var adapter = require('tower-adapter')
   , container = require('tower-container')
+  , model = require('tower-model')
+  , strcase = require('tower-strcase')
+  // XXX: strip out pluralize/singularize into single
+  //      function components.
+  , inflection = require('inflection')
   , MemoryAdapter = adapter('memory');
 
 /**
@@ -15,3 +20,35 @@ module.exports = MemoryAdapter;
 
 MemoryAdapter
   .type('string');
+
+MemoryAdapter.prototype.initialize = function(){
+  if (this.initialized) return;
+  this.recordsByType = {};
+  this.initialized = true; // XXX: tmp hack
+}
+
+/**
+ * Parse the `criteria` object and execute.
+ *
+ * @param {Array}     criteria
+ * @param {Function}  fn
+ * @api public
+ */
+
+MemoryAdapter.prototype.execute = function(criteria, fn){
+  this.initialize();
+  // assuming for now that all criteria start with ['start', collectionName]
+  // and end with an action. Then, all intermediate criterion are conditions.
+  // XXX: will expand in a bit.
+
+  var action = criteria[criteria.length - 1];
+
+  this[action[1]](criteria, fn);
+}
+
+MemoryAdapter.prototype.insert = function(criteria, fn){
+  var start = criteria[0];
+  var Model = model(inflection.singularize(start[1]));
+
+  fn(null, this.recordsByType[0] = criteria.pop()[2]);
+}
--- test/index.js
+++ test/index.js
@@ -1,8 +1,24 @@
 var MemoryAdapter = require('..')
+  , adapter = require('tower-adapter')
+  , container = require('tower-container')
+  , model = require('tower-model')
   , assert = require('chai').assert;
 
 describe('memoryAdapter', function() {
-  it('should define', function() {
-    
+  var Post = model('post')
+    .attr('title', 'string');
+
+  it('should execute criteria', function(done) {
+    var post = new Post({ title: 'foo' });
+
+    var criteria = [
+        ['start', 'posts']
+      , ['action', 'insert', post]
+    ];
+
+    adapter('memory').execute(criteria, function(){
+      console.log(arguments);
+      done();
+    });
   });
 });
