7b6d09f8b25228f4bf70d5c835bc6defbe3fb9c2
Lance Pollard <lancejpollard@gmail.com>
2013-03-05 23:49:17 -0800

    ideas

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ MemoryAdapter
 
 MemoryAdapter.prototype.initialize = function(){
   if (this.initialized) return;
-  this.recordsByType = {};
+  //this.recordsByType = {};
   this.initialized = true; // XXX: tmp hack
 }
 
@@ -51,4 +51,39 @@ MemoryAdapter.prototype.insert = function(criteria, fn){
   var Model = model(inflection.singularize(start[1]));
 
   fn(null, this.recordsByType[0] = criteria.pop()[2]);
+}
+
+var operations = {};
+
+// XXX: tower-operator module
+operations['string.eq'] = function(a, b){
+  return a === b;
+}
+
+function operation(type, operator, a, b){
+  return operations[type + '.' + operator](a, b);
+}
+
+MemoryAdapter.prototype.query = function(criteria, fn){
+  var type = criteria[0][1]
+    , collection = this.recordsByType[type]
+    , result = []
+    , condition
+    , attrs = model(inflection.singularize(type)).attrs
+    , record;
+
+  for (var i = 1, n = collection.length - 1; i < n; i++) {
+    record = collection[i];
+    for (var j = 1, m = criteria.length - 1; j < m; j++) {
+      // [ 'condition', 'eq', 'title', 'two' ]
+      condition = criteria[j];
+      // XXX: need to get field type from schema.
+      var attrType = attrs[condition[2]].type;
+      // XXX: this isn't correct yet, only accounts for one condition.
+      if (operation(attrType, condition[1], record[condition[2]], condition[3]))
+        result.push(record);
+    }
+  }
+
+  fn(null, result);
 }
--- test/index.js
+++ test/index.js
@@ -4,11 +4,32 @@ var MemoryAdapter = require('..')
   , model = require('tower-model')
   , assert = require('chai').assert;
 
-describe('memoryAdapter', function() {
-  var Post = model('post')
-    .attr('title', 'string');
+var database = {
+  posts: [
+      { id: 1, title: 'post one' }
+    , { id: 2, title: 'post two' }
+    , { id: 3, title: 'post three' }
+  ],
 
-  it('should execute criteria', function(done) {
+  comments: [
+      { id: 10, message: 'comment one', userId: 100, embeddedIn: 'post' } // embedded
+    , { id: 10, message: 'comment one', userId: 101, postId: 1 } // referenced
+  ],
+
+  users: [
+      { id: 100, email: 'user100@email.com' }
+    , { id: 101, email: 'user101@email.com' }
+  ]
+}
+
+adapter('memory').prototype.recordsByType = database;
+
+describe('memoryAdapter', function(){
+  var Post = model('post')
+    .attr('title', { type: 'string' });
+
+  /*
+  it('should execute criteria', function(done){
     var post = new Post({ title: 'foo' });
 
     var criteria = [
@@ -17,8 +38,22 @@ describe('memoryAdapter', function() {
     ];
 
     adapter('memory').execute(criteria, function(){
-      console.log(arguments);
       done();
     });
   });
+  */
+
+  it('should query', function(done){
+    var criteria = [
+        ['start', 'posts']
+      , ['condition', 'eq', 'title', 'post two']
+      , ['action', 'query']
+    ];
+
+    adapter('memory').execute(criteria, function(err, records){
+      assert.equal(1, records.length);
+      assert.deepEqual({ id: 2, title: 'post two' }, records[0]);
+      done();
+    });
+  })
 });
